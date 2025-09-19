39ba84bde0eea861c96a781b15f7e28b5a821b7a
Daniel Engvall <daniel.engvall@amelia.com>
2025-09-19 12:03:14 +0200

    Make 'tags' column sortable in task table and bump to v2025.9.8

--- src/jrnl/tasks/table.py
+++ src/jrnl/tasks/table.py
@@ -6,7 +6,7 @@ def day_summaries_to_table(tasks: list[DaySummaryDict]) -> ui.table:
     columns = [
     {'name': 'date', 'label': 'Date', 'field': 'date', 'required': True, 'align': 'left', 'sortable': True},
     {'name': 'total_time', 'label': 'Time', 'field': 'total_time', 'required': True, 'align': 'left', 'sortable': True},
-    {'name': 'tags', 'label': 'Tags', 'field': 'tags', 'required': True, 'align': 'left'},
+    {'name': 'tags', 'label': 'Tags', 'field': 'tags', 'required': True, 'align': 'left', 'sortable': True},
     {'name': 'text_summary', 'label': 'Title', 'field': 'text_summary', 'required': True, 'align': 'left'},
     {'name': 'task_ids', 'label': 'Task IDs', 'field': 'task_ids', 'required': True, 'align': 'left'},
     {'name': 'starred', 'label': 'Starred', 'field': 'starred', 'required': True, 'align': 'left', 'sortable': True},
